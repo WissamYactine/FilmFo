462fc15784d638d650d370d4d3b33a6ee37a3ba5
Wissam Yactine <wissam.professionnel@gmail.com>
2024-09-05 14:00:09 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use(cors());
 app.use("/movies", moviesRoutes);
 app.use("/user", userRoutes);
 
-app.get('/', (res, req) => {
-    res.send('APP IS RUNNING.');
-})
+// app.get('/', (res, req) => {
+//     res.send('APP IS RUNNING.');
+// })
 
 const PORT = process.env.PORT || 5000;
 
